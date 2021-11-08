8ddbe06689d7a2d80e3a34515a553dc9b48bcd77
Jasper Chen <jasperdyb@gmail.com>
2021-11-08 13:06:12 +0800

    Add pacakges

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+
+const CustomizedButton = styled(Button)`
+  background-color: green;
+  color: red;
+`;
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>NextJS Blog with Butter CMS</title>
+        <title>Hello World</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1> NextJS Blog with Butter CMS</h1>
+      <h1> Hello World</h1>
+      <CustomizedButton variant="text">Text</CustomizedButton>
     </div>
   );
 };
